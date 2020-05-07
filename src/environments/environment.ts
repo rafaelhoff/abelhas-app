f9998aae6ec6f2183ba1f65d822ff3eda3e71cac
rafaelhoff <rafael.hoff@gmail.com>
2020-05-07 20:47:25 +0200

    fix(env): connect to cognito to false

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
   production: false,
-  connectToCognito: true,
+  connectToCognito: false,
   connectToS3: false,
   defaultPicture: '/assets/img/profile.png',
   mockData: false
